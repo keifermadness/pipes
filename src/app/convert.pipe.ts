2c4b161a9208565bded2e4c4d9ba6bf04ffb4477
keifermadness <keifergilbert@gmail.com>
2022-06-06 16:40:33 -0700

    added in custom ConvertPipe functionality

---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert'
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: any, targetUnits: string) {
+    if (!value) {
+      return '';
+    }
+
+    switch(targetUnits) {
+      case 'km': 
+        return value * 1.60934;
+      case 'm': 
+        return value * 1.60934 * 1000
+      case 'cm':
+        return value * 1.60934 * 1000 * 100;
+      default:
+        throw new Error('Target unit not supported, please enter another value')
+    }
+
+    return value * 1.60934;
+  }
+
+}
